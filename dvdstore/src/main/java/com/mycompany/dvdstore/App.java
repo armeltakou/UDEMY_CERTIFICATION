2d623b053573eda86b72898370d4297d1c3215f0
Armel Takou <armeltakou@gmail.com>
2024-05-23 02:58:21 +0100

    EXPLOITATION DU CONTENU LEGER SPRING
    Exercice 6 DVDStore

--- dvdstore/src/main/java/com/mycompany/dvdstore/App.java
+++ dvdstore/src/main/java/com/mycompany/dvdstore/App.java
@@ -1,20 +1,25 @@
 package com.mycompany.dvdstore;
 
-import com.mycompany.dvdstore.controller.MovieController;
-import com.mycompany.dvdstore.repository.FileMovieRepository;
-import com.mycompany.dvdstore.service.DefaultMovieService;
+import com.mycompany.dvdstore.controller.MovieControllerInterface;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
     public static void main(String[] args){
         System.out.println("Bienvenue dans ma galerie de film");
+
+        /*
         MovieController movieController = new MovieController();
 
         DefaultMovieService movieService = new DefaultMovieService();
         movieController.setMovieService(movieService);
         FileMovieRepository movieRepository = new FileMovieRepository();
         movieService.setMovieRepositoryInterface(movieRepository);
+        */
 
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        MovieControllerInterface movieController = applicationContext.getBean(MovieControllerInterface.class);
         movieController.addMovie();
     }
 
