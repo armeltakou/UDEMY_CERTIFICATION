4cb4f94ff2b24ff366066a0e0cc9da62a13eb028
Armel Takou <armeltakou@gmail.com>
2024-05-31 09:20:06 +0100

    UPDATE SECTION 4 (EXERCICE 14 ET 15)
    Déploiement d'une application en production (utilisation de Maven)...

--- dvdstore/src/main/java/com/mycompany/dvdstore/App.java
+++ dvdstore/src/main/java/com/mycompany/dvdstore/App.java
@@ -1,31 +1,16 @@
 package com.mycompany.dvdstore;
 
 import com.mycompany.dvdstore.controller.MovieControllerInterface;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
-@Configuration
-@ComponentScan(basePackages = {"com.mycompany.dvdstore.controller", "com.mycompany.dvdstore.repository.file", "com.mycompany.dvdstore.service"})
-@PropertySource("classpath:application.properties")
+@SpringBootApplication
 public class App {
 
     public static void main(String[] args){
         System.out.println("Bienvenue dans ma galerie de film");
-
-        /*
-        MovieController movieController = new MovieController();
-
-        DefaultMovieService movieService = new DefaultMovieService();
-        movieController.setMovieService(movieService);
-        FileMovieRepository movieRepository = new FileMovieRepository();
-        movieService.setMovieRepositoryInterface(movieRepository);
-        */
-
-        //ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(App.class);
+        ApplicationContext applicationContext = SpringApplication.run(App.class, args);
         MovieControllerInterface movieController = applicationContext.getBean(MovieControllerInterface.class);
         movieController.addMovie();
     }
